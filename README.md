3ce523eae95394b2ffde3fd6798befa163b83343
MattHughson <matthew.hughson@lesappelsillimites.com>
2024-02-22 22:17:46 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# The Storyblok Nuxt Ultimate Tutorial: Part 1
+adding text
---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  css: ['@/assets/css/roboto.css'],
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+        accessToken: 'W1vLyxT5rQ15jBpANjnv0gtt',
+        apiOptions: {
+          region: '' // Set 'US" if your space is created in US region (EU default)
+        }
+      },
+    ],
+    '@nuxtjs/tailwindcss',
+  ]
+})
