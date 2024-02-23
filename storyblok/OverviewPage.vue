9a579bf79f4d5e11ccaed43c296345201033394a
Alex Rogers <37906277+dfex01@users.noreply.github.com>
2024-02-23 18:26:02 +0000

    test add overview page blok

--- storyblok/OverviewPage.vue
+++ storyblok/OverviewPage.vue
@@ -1,5 +1,7 @@
 <template>
-  Test
+  <div v-editable="blok" class="px-4">
+    <StoryblokComponent v-for="blok in blok.body" :key="blok._uid" :blok="blok" />
+  </div>
 </template>
 
 <script setup>
