96ec7ee2337e2c64a4c941c4c757700ce25a4973
Alex Rogers <37906277+dfex01@users.noreply.github.com>
2024-02-29 14:59:15 +0000

    email newsletter init commit

---
+++ pages/february-email.vue
@@ -0,0 +1,7 @@
+<script setup>
+const story = await useAsyncStoryblok('february-email', { version: 'draft' })
+</script>
+ 
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
---
+++ storyblok/EmailNewsletter.vue
@@ -0,0 +1,26 @@
+<style>
+    @import "assets/css/overviewRedesign.css"
+</style>
+
+<template>
+    <div class="banner">
+        <img :src="blok.banner.filename" />
+    </div>
+  <div v-editable="blok" v-html="headlineHtml">
+  </div>
+  <div v-editable="blok" v-html="footerHtml">
+  </div>
+</template>
+
+<script setup>
+    import { onMounted, computed } from 'vue';
+    import { renderRichText } from '@storyblok/vue';
+
+    const props = defineProps({ blok: Object })    
+
+    console.log("props.blok: ", props.blok)
+
+    const headlineHtml = renderRichText(props.blok.headline);
+    const footerHtml = renderRichText(props.blok.footer);
+
+</script>
