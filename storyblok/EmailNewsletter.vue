c17e7f63c5ff5f09f53456a978b7db8d29da0d31
Alex Rogers <37906277+dfex01@users.noreply.github.com>
2024-02-29 15:37:34 +0000

    -email work -file organization

--- storyblok/EmailNewsletter.vue
+++ storyblok/EmailNewsletter.vue
@@ -1,15 +1,35 @@
-<style>
-    @import "assets/css/overviewRedesign.css"
+<style scoped>
+
+    .email-wrapper {
+       width:100%;
+       max-width:800px;
+    }
+    .adc-img {
+        display: block;
+        color: #000000;
+        text-decoration: none;
+        font-family: Helvetica, arial, sans-serif;
+        font-size: 16px;
+        max-width: 80% !important;
+        width: 80%;
+        height: auto !important;
+    }
 </style>
 
 <template>
-    <div class="banner">
-        <img :src="blok.banner.filename" />
-    </div>
-  <div v-editable="blok" v-html="headlineHtml">
-  </div>
-  <div v-editable="blok" v-html="footerHtml">
-  </div>
+    <div class="email-wrapper mx-auto">
+        <img src="https://cdn.automationdirect.com/static/images/logos/1024x154.png"
+            class="adc-img" />
+        <div class="issue-no">
+            Newsletter Volume {{ blok.volume }} Issue {{ blok.issue }}
+        </div>
+        <div class="banner">
+            <img :src="blok.banner.filename" />
+        </div>
+
+        <div v-editable="blok" v-html="headlineHtml"></div>
+        <div v-editable="blok" v-html="footerHtml"></div>
+    </div>    
 </template>
 
 <script setup>
