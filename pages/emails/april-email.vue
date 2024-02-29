4bc1a14c41859f8c009ad04048957b4c95aef4a1
Alex Rogers <37906277+dfex01@users.noreply.github.com>
2024-02-29 18:04:38 +0000

    add a couple of test pages for email

---
+++ pages/emails/april-email.vue
@@ -0,0 +1,7 @@
+<script setup>
+const story = await useAsyncStoryblok('emails/april-email', { version: 'draft' })
+</script>
+ 
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
